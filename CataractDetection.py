d204815ec67cca6e56c6bcb2fe366811faf8c61c
Aqsa Kausar <aqsakausar30@gmail.com>
2018-01-21 00:44:50 +0500

    Update CataractDetection.py

--- CataractDetection.py
+++ CataractDetection.py
@@ -36,8 +36,8 @@ circles = cv2.HoughCircles(morpho, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=
 
 #Traverse over the circles
 for i in circles[0,:]:
-    cv2.circle(cimg_morpho,(i[0],i[1]),i[2],(0,255,0),2)        # draw the outer circle
-    cv2.circle(cimg_morpho,(i[0],i[1]),2,(0,0,255),3)           # draw the center of the circle
+    cv2.circle(cimg_morpho,(i[0],i[1]),i[2],(0,255,0),2)        # Draw the outer circle
+    cv2.circle(cimg_morpho,(i[0],i[1]),2,(0,0,255),3)           # Draw the center of the circle
 
 cv2.imshow("5 - Find Circles", cimg_morpho)                     # Display the image with circles detected
 img_morpho_copy = morpho.copy()                                 # Copy the image resulting from morphological opening
@@ -52,9 +52,7 @@ for i in range(cols):                                           # Traverse over 
             img_morpho_copy[j,i] = 0                            # Convert the pixel to black
 
 imgg_inv = cv2.bitwise_not(img_morpho_copy)                     # Bitwise Not, to convert black to white and white to black
-
-# cv2.imwrite("iris.jpg",img_morpho_copy)
-cv2.imshow("8 - Iris Contour Separation", img_morpho_copy)                 # Display image after morphological opening
+cv2.imshow("8 - Iris Contour Separation", img_morpho_copy)      # Display image after morphological opening
 cv2.imshow("7 - Image Inversion", imgg_inv)                     # Display image after detection and separation of circle - this is our mask
 
 
@@ -64,12 +62,12 @@ cimg_pupil = img.copy()                                                         
 for cnt in contours0:                                                   # Traverse over the contours
         cv2.drawContours(cimg_pupil, cnt, -1, (0, 255, 0), 3, 8)        # Draw the contours boundary on the original image copy
         pupil_area = cv2.contourArea(cnt)                               # Find area of contour
-        print "Pupil area: ", pupil_area                                                # Print area of contour
+        print "Pupil area: ", pupil_area                                # Print area of contour
         M = cv2.moments(cnt)                                            # Find moments of contour
         cX_pupil = int(M["m10"] / M["m00"])                             # Use moments to compute centre of mass - x coordinate
         cY_pupil = int(M["m01"] / M["m00"])                             # Use moments to compute centre of mass - y coordinate
         cv2.circle(cimg_pupil, (cX_pupil, cY_pupil), 2, (0, 0, 255), -1)# Draw a point(circle radius 2) on the image at the centre of mass computed
-        print ("Centre of pupil: (%d,%d)") % (cX_pupil, cY_pupil)      # Print the Centre of mass of pupil
+        print ("Centre of pupil: (%d,%d)") % (cX_pupil, cY_pupil)       # Print the Centre of mass of pupil
 
 cv2.imshow("8 - Iris Detected", cimg_pupil)                             # Display the image with the centre of mass of pupil drawn
 cv2.imwrite("cimg_pupil.jpg",cimg_pupil)                                # Save this image
@@ -86,11 +84,11 @@ for cnt in contours0:                                                   # Traver
             cX_cat = int(M["m10"] / M["m00"])                           # Use moments to compute centre of mass - x coordinate
             cY_cat = int(M["m01"] / M["m00"])                           # Use moments to compute centre of mass - y coordinate
             cv2.circle(cimg_cat, (cX_cat, cY_cat), 2, (0, 0, 255), -1)  # Draw point on the image at the centre of cataract
-            print ("Centre of cataract: (%d,%d)") % (cX_cat, cY_cat)   # Print the centre of cataract
+            print ("Centre of cataract: (%d,%d)") % (cX_cat, cY_cat)    # Print the centre of cataract
             # Calculate Difference between pupil centre and cataract centre
             print ("Cataract is (%d,%d) away from pupil centre") % (cX_cat - cX_pupil, cY_cat - cY_pupil)
 
-cv2.imshow("9 - Cataract Detected", cimg_cat)                            # Display the cataract contour on the original image copy
+cv2.imshow("9 - Cataract Detected", cimg_cat)                           # Display the cataract contour on the original image copy
 cataract_percentage = (cat_area / (pupil_area + cat_area)) * 100        # Calculate percentage of cataract developed
 print ("You have %.2f percent cataract") % (cataract_percentage)        # Display percentage of cataract developed
 
